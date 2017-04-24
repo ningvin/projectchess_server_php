1f1cf845fd5f1ed0e625c937c30e50b7b7394352
mtpns <louisse.mary@gmail.com>
2017-04-24 15:30:59 +0100

    very basic screen

--- Login.php
+++ Login.php
@@ -1,15 +1,32 @@
+<?php
+
+	session_start();
+?>
+
+<html>
+
+<head>
+	<title>Chess Game LogIn</title>
+	<link href = "Login.css" rel = "stylesheet">
+</head>
+<body>
+
+<center>
+
+<h2>Sign in or Register here</h2>
 <form action="/action_page.php">
   <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    <img src="https://cdn3.iconfinder.com/data/icons/brain-games/1042/Chess-Game.png alt="Chess Icon" class="avatar">
   </div>
 
   <div class="container">
     <label><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
+	</br>
 
     <label><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
-
+	</br>
     <button type="submit">Login</button>
     <input type="checkbox" checked="checked"> Remember me
   </div>
@@ -19,3 +36,7 @@
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
+</center>
+</body>
+
+</html>
